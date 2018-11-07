f6461355c5170b5ddbc30f36d77de43d7dced0c8
Brendan McCray <rbmccr@gmail.com>
2018-11-06 20:12:23 -0600

    json object now contains mood parent element

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,6 +1,7 @@
 //fetches JSON data from local database and passes data into function
 const API = {
+  // the mood parent resource is added to the JSON object
   getJournalEntries () {
-    return fetch("http://localhost:3000/journalEntries").then(jsonData => jsonData.json())
+    return fetch("http://localhost:3000/journalEntries?_expand=mood").then(jsonData => jsonData.json()).then(data => console.log(data))
   }
 }
